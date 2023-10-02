cdcdd196d40e24023acebedbdc95a63ce15f5935
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2023-10-02 15:27:06 -0400

    removed fetch function

--- index.ts
+++ index.ts
@@ -1,44 +1,30 @@
-interface customerPurchaseData {
-    id: number,
-    firstName: string,
-    lastName: string,
-    purchasedDate: string,
-    lastPaymentDate: string,
-    phone: string,
-    make: string,
-    model: string,
-    city: string
-}
-
-export async function fetchData():Promise<customerPurchaseData> {
-    // waits for promise to resolve
-    const res = await fetch('data.json');
-    const data: customerPurchaseData = await res.json();
-    // Returns json wrapped in a promise!
-    return data;
-}
-
+const data = require('./data.json');
 import * as S from 'strinify';
-
-export function fullName(firstName: string, lastName: string): string {
-    const fullName = `${S.capitalizeWords(firstName)} ${S.capitalizeWords(lastName)}`
-    return fullName 
-}
-
 const D = require('dates-in-js');
 
-export function formatPurchasedDate(purchasedDate: string): string {
-    const purchased = new D(purchasedDate)
-    const formattedPurchaseDate = purchased.format('M D, Y')
-    return `Purchased: ${formattedPurchaseDate}`
+
+export function fullName(firstName: string, lastName: string): string {
+    const formattedFullName: string = `${S.capitalizeWords(firstName)} ${S.capitalizeWords(lastName)}`;
+    return formattedFullName;
 }
+
+console.log(fullName(data[0].first_name, data[0].last_name));
+
+export function formatPurchasedDate(purchasedDate: string): string {
+    const purchased = new D(purchasedDate);
+    const formattedPurchaseDate = purchased.format('M D, Y');
+    return `Purchased: ${formattedPurchaseDate}`;
+}
+
+console.log(formatPurchasedDate(data[0].purchased));
 
 export function lastPaymentWhen(lastPaymentDate: string): string {
-    const lastPayment = new D(lastPaymentDate)
-    const when = lastPayment.when()
-    return `Last Payment: ${when}`
+    const lastPayment = new D(lastPaymentDate);
+    const when = lastPayment.when();
+    return `Last Payment: ${when}`;
 }
 
+console.log(lastPaymentWhen(data[0].lastpayment));
 
 export function formatPhone(phoneNumber: number | string): string {
     const phone = typeof phoneNumber === 'number' ? phoneNumber.toString() : phoneNumber;
@@ -46,11 +32,14 @@ export function formatPhone(phoneNumber: number | string): string {
         const formattedPhoneNumber = `(${phone.slice(0,3)}) ${phone.slice(3, 6)}-${phone.slice(6)}`;
         return formattedPhoneNumber;
     }
-    throw "Error: The phone number should be 10 digit number or a 10 character string"
+    throw "Error: The phone number should be 10 digit number or a 10 character string";
 }
 
+console.log('Phone Number:', formatPhone(data[0].phone));
+
+
 const challenge = {
-    fetchData, formatPhone, formatPurchasedDate, lastPaymentWhen, fullName
+    formatPhone, formatPurchasedDate, lastPaymentWhen, fullName
 }
 
 export default challenge;
